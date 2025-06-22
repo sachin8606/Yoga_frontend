108f22aa14654315bc8c8a6f7727a3a80ee7055f
sachin <sachin.rangar8606@gmail.com>
2025-06-23 01:36:34 +0530

    removed useEffect from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import YogaLogo from './YogaLogo';
